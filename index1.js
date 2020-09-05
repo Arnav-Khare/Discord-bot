ede0c7e0823414df61dd6a4bd22e98955ffa0faa
Arnav-Khare <55575931+Arnav-Khare@users.noreply.github.com>
2020-09-06 02:58:11 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
   
-const token = '';   
+const token = flame;   
  
 bot.on('ready', () => {
     console.log('This bot is active!');
