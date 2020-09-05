9cbb77b41addfe38b35e7a83b0be68632726b1b3
Arnav Khare <rockystar197@gmail.com>
2020-09-06 01:52:39 +0530

    90 percent done

---
+++ index1.js
@@ -0,0 +1,79 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+  
+const token = 'NzIxNDU1NjkyOTA0NTI5OTgw.XuU03Q.Suo1NlVRwYV9kNOxIPzxWO2kDjo';   
+ 
+bot.on('ready', () => {
+    console.log('This bot is active!');
+})
+ 
+bot.on('message',message=>{
+    if(message.content==='!command')
+    {
+        message.channel.send('Below are the list of following command...type the following commands to see what bot can do!!! Hello\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tAao\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tPlanA\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tGalti word in a sentence ')
+
+    }
+})
+bot.on('message', message => {
+    if(message.author.bot === true)
+    {
+        return 
+    }
+    if(message.content === 'Hello')
+         message.channel.send(`Hello ${message.author}..How are you!!!`)
+         
+        if(message.content==='Aao')
+         {
+             message.channel.send('Its pubg time baby!!!')
+            var arr1  = message.guild.members.cache;  //return the all the members in a guild
+            arr1.forEach(e => {
+                if((e.user === message.author)||(e.user.bot === true)||e.user.username==='Amannjalan')
+                {
+
+                }   
+                else
+                {
+                    message.channel.send(`Aaoo ${e.user} online ........`)
+                }
+            })
+
+         }
+         if(message.content.includes('galti'))
+         {
+             message.channel.send(`Aaare Vo pubg ki Galti hai ${message.author}!!!!`)
+         }
+ 
+    if(message.content==='Plan a')
+    {
+            var arr  = message.guild.members.cache;  //return the all the members in a guild
+            var person=arr.find(e => e.user.username==='deeksha')
+            
+            if(!person) return  message.reply("I CANT FIND THE USER ")
+            
+            let mainrole = message.guild.roles.cache.find(role => role.name === "Jarvis");
+            let role = message.guild.roles.cache.find(role => role.name === "Deeksha");
+        
+            if(!role) return message.reply("Couldn't find the mute role.")
+
+            let time = 20000;
+
+            person.roles.remove(mainrole);
+            person.roles.add(role);
+            
+            message.channel.send('Fuck Off deeksha!!!!')
+ 
+            setTimeout(function(){
+               
+                person.roles.add(mainrole)
+                person.roles.remove(role);
+                message.channel.send(`@${person.user.tag} has been unmuted.`)
+            }, time);
+
+    }
+ 
+ 
+
+});
+bot.login(token);
+
+
---
+++ index2.js
@@ -0,0 +1,140 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+const bot = new Discord.Client();
+
+const queue = new Map();
+
+const token = 'NzUxMzY3NjA2NDI2MDA5NjEy.X1IDnQ._DTPaphQgLPvspt2gG3Ljtj4Si0' //require token for discord 
+
+
+bot.on('ready', () => {
+    console.log('Music Bot is online');
+})
+
+bot.on('message' ,(message)=> {
+    switch(message){
+        case '!command':
+            message.channel.send('Here are the list of following command ')
+    }
+});
+
+bot.on('message' ,async (message)=>{
+
+    if(message.author.bot)          //to check whether message is not sent by bot..........
+    {
+        return;
+    }
+    let args = message.content.substring(1).split(' ');
+    const serverQueue = queue.get(message.guild.id) //message,guild.id -> Arnav's server id4
+
+    switch(args[0]){
+        case 'p':
+            const voiceChannel = message.member.voice.channel; //voiceChannel -> returns a object conating the voice channel in which the message author is present
+            if(!voiceChannel)
+            {
+                message.reply('please join voice channel')
+                return
+            }
+            const songInfo =  await ytdl.getInfo(args[1])  
+            const song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url
+            }
+            if(!serverQueue) {              //Creating object to be valued for the key(key is the id of my server)
+                const queueConstruct = {
+                    textChannel: message.channel,
+                    voiceChannel: voiceChannel,
+                    connection: null,
+                    songs: [],
+                    volume: 5,
+                    playing: true
+                }
+            
+            queue.set(message.guild.id,queueConstruct)      //Mapping key value pair
+
+            queueConstruct.songs.push(song )        //adding song to array of songs 
+            try {
+                var connection = await voiceChannel.join()  //connection === promise
+                queueConstruct.connection = connection
+                play(message.guild, queueConstruct.songs[0])
+            } catch (err){
+                console.log(`there was a error connecting to this channel ${err}`)
+                queue.delete(message.guild.id)
+                message.channel.send('Connection error');
+            }
+         }
+          else{
+            serverQueue.songs.push(song)
+            return message.channel.send(`**${song.title} has been added to queue`);
+           }
+           break;
+        case 's' :
+            if(!serverQueue)
+            return (
+                message.channel.send(`nothing in queue`)
+            )
+            serverQueue = []
+            serverQueue.connection.dispatcher.end()
+            message.channel.send('Stopped ')
+
+
+            return undefined;
+        case 'np' :
+            const voiceChannel1 = message.member.voice.channel; //voiceChannel -> returns a object conating the voice channel in which the message author is present
+            if(!voiceChannel1)
+            {
+                message.reply('please join voice channel')
+                return
+            }
+            if(!serverQueue)
+            {
+                message.channel.send('Nothing is playing');
+                return
+            }
+            message.channel.send(`
+            __**Currently Playing**__
+            ${serveQueue.songs[0].title} `);
+            return undefined;
+        case 'q': 
+             const voiceChannel2 = message.member.voice.channel; //voiceChannel -> returns a object conating the voice channel in which the message author is present
+            if(!voiceChannel2)
+            {
+            message.reply('please join voice channel')
+            return
+            }
+            if(!serverQueue){
+                message.channel.send('Nothing in the queue');
+                return
+            }
+            message.channel.send(`
+            __**Songs-Queue:**__
+            ${serverQueue.songs.map(song => `**-**${song.title}`).join('\n')}`)
+            
+        }
+    }
+)
+
+function play(guild, song){
+    const serverQueue = queue.get(guild.id)
+
+    
+    if(!song)       //reccursion base case 
+    {
+        serverQueue.voiceChannel.leave()
+        queue.delete(guild.id)
+        return undefined
+    }
+    const dispatcher = serverQueue.connection.play(song.url)
+    .on( 'finish',() =>{
+        serverQueue.songs.shift()
+        play(guild , serverQueue.songs[0])
+    })
+    .on('error',error =>{
+        console.log(error)
+    })
+    dispatcher.setVolumeLogarithmic( serverQueue.volume / 5)
+}
+
+bot.login(token);
+
